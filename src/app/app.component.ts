be2c8c18075c21c803fa3d63e2c3bcfcca7fe22d
acexode <abudawud92@gmail.com>
2020-11-08 22:54:04 +0100

    form submit events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,18 +13,19 @@ import { UserService } from "./user.service";
 })
 export class AppComponent {
   form: FormGroup;
-
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
   constructor(private userService: UserService, private fb: FormBuilder) {
     this.buildForm();
   }
-
+  submitted = false;
+  userCreated = false;
   buildForm(): void {
     this.form = this.fb.group({
       firstname: ["", [Validators.required, Validators.minLength(3)]],
       lastname: ["", [Validators.required, Validators.minLength(3)]],
       email: [
         "",
-        [Validators.required, Validators.minLength(3)],
+        [Validators.required, Validators.pattern(this.emailPattern)],
         this.isEmailUnique.bind(this) // async Validator passed as 3rd parameter
       ],
       password: ["", [Validators.required, Validators.minLength(5)]]
@@ -50,8 +51,15 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.form.value);
+    this.submitted = true;
     this.userService.signup(this.form.value).subscribe((e) => {
       console.log(e);
+      this.submitted = false;
+      this.userCreated = true;
+      setTimeout(() => {
+        this.userCreated = false;
+      }, 5000);
+      this.form.reset();
     });
   }
 }
